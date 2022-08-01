b28c2fcf2f1d89489cde0bd13655dabd8bdbff96
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-01 05:31:38 +0200

    E7746: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: micromax Model: IN_Note1 Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_E7746.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_E7746-user \
+    omni_E7746-userdebug \
+    omni_E7746-eng
